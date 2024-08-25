1d298223b27bc07eb71207459c5a88360e4b37ff
To Quang Duc <toquangduc2004@gmail.com>
2024-08-25 17:16:43 +0700

    tích hợp đăng kí- đăng nhập

--- lib/pages/home/user_home.dart
+++ lib/pages/home/user_home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/login.dart';
+import 'package:flutter_application_1/pages/loginregister/login.dart';
 
 class UserHomePage extends StatelessWidget {
   const UserHomePage({super.key});
